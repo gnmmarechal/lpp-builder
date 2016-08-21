8aca261eed4a4330981928f70dc67101b48a95ac
gnmmarechal <gnmpolicemata91@gmail.com>
2016-08-21 15:55:07 +0100

    Initial version. Should work.

---
+++ Lua Player Plus Builder/Lua Player Plus Builder/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Lua_Player_Plus_Builder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "App Name";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(735, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Title ID";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(151, 109);
+            this.textBox2.MaxLength = 4;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(263, 31);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(551, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 76);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Build VPK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 167);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lua Player Plus Builder GUI v1.0 - gnmmarechal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lua Player Plus Builder/Lua Player Plus Builder/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace Lua_Player_Plus_Builder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Changing title ID to uppercase
+            textBox2.Text = textBox2.Text.ToUpper();
+
+            //Save VPK dialog
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.FileName = textBox1.Text + ".vpk";
+            savefile.Filter = "HENkaku Application File (*.vpk)|*.vpk|All files (*.*)|*.*";
+
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                //Building the VPK
+                string appname = textBox1.Text;
+                string titleid = textBox2.Text;
+                string vpkname = savefile.FileName;
+
+                //Checks for the existence of required programs, if not, unpack them.
+                if (!File.Exists("vita-mksfoex.exe"))
+                {
+                    File.WriteAllBytes("vita-mksfoex.exe", Lua_Player_Plus_Builder.Properties.Resources.vita_mksfoex);
+                }
+
+                //Make the param.sfo
+                ProcessStartInfo startmksfoex = new ProcessStartInfo();
+                startmksfoex.Arguments = "-s TITLE_ID="+ titleid + "00001 \"" + appname + "\" param.sfo";
+                startmksfoex.FileName = "vita-mksfoex.exe";
+                startmksfoex.WindowStyle = ProcessWindowStyle.Hidden;
+                startmksfoex.CreateNoWindow = true;
+                int exitCode;
+                using (Process proc = Process.Start(startmksfoex))
+                {
+                    proc.WaitForExit();
+                    exitCode = proc.ExitCode;
+                }
+
+                //Copy the param.sfo to the LPP dir.
+                if (File.Exists("build/sce_sys/param.sfo"))
+                    File.Delete("build/sce_sys/param.sfo");
+                File.Move("param.sfo", "build/sce_sys/param.sfo");
+
+                //Pack the VPK
+                ZipFile.CreateFromDirectory("build", vpkname);
+                if (File.Exists(vpkname))
+                    MessageBox.Show("Success! Created " + vpkname + ".");
+            }
+        }
+    }
+}
