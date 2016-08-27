04c9111ad67c24eddc776b82caf6fb6b91fe74c8
gnmmarechal <gnmpolicemata91@gmail.com>
2016-08-27 20:52:58 +0100

    Getting UI ready for multiple platforms.

--- Lua Player Plus Builder/Lua Player Plus Builder/Form1.Designer.cs
+++ Lua Player Plus Builder/Lua Player Plus Builder/Form1.Designer.cs
@@ -32,9 +32,20 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.targetPlaftormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playStationVitaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hENkakuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lPPR4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nintendo3DSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dSXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cIAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dSToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
@@ -56,11 +67,88 @@
             // menuStrip1
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(735, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(761, 40);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.targetPlaftormToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(113, 36);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // targetPlaftormToolStripMenuItem
+            // 
+            this.targetPlaftormToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playStationVitaToolStripMenuItem,
+            this.nintendo3DSToolStripMenuItem});
+            this.targetPlaftormToolStripMenuItem.Name = "targetPlaftormToolStripMenuItem";
+            this.targetPlaftormToolStripMenuItem.Size = new System.Drawing.Size(269, 38);
+            this.targetPlaftormToolStripMenuItem.Text = "Target";
+            // 
+            // playStationVitaToolStripMenuItem
+            // 
+            this.playStationVitaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hENkakuToolStripMenuItem});
+            this.playStationVitaToolStripMenuItem.Name = "playStationVitaToolStripMenuItem";
+            this.playStationVitaToolStripMenuItem.Size = new System.Drawing.Size(280, 38);
+            this.playStationVitaToolStripMenuItem.Text = "PlayStation Vita";
+            // 
+            // hENkakuToolStripMenuItem
+            // 
+            this.hENkakuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lPPR4ToolStripMenuItem});
+            this.hENkakuToolStripMenuItem.Name = "hENkakuToolStripMenuItem";
+            this.hENkakuToolStripMenuItem.Size = new System.Drawing.Size(212, 38);
+            this.hENkakuToolStripMenuItem.Text = "HENkaku";
+            // 
+            // lPPR4ToolStripMenuItem
+            // 
+            this.lPPR4ToolStripMenuItem.Name = "lPPR4ToolStripMenuItem";
+            this.lPPR4ToolStripMenuItem.Size = new System.Drawing.Size(186, 38);
+            this.lPPR4ToolStripMenuItem.Text = "LPP R4";
+            // 
+            // nintendo3DSToolStripMenuItem
+            // 
+            this.nintendo3DSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dSToolStripMenuItem});
+            this.nintendo3DSToolStripMenuItem.Name = "nintendo3DSToolStripMenuItem";
+            this.nintendo3DSToolStripMenuItem.Size = new System.Drawing.Size(280, 38);
+            this.nintendo3DSToolStripMenuItem.Text = "Nintendo 3DS";
+            // 
+            // dSToolStripMenuItem
+            // 
+            this.dSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dSXToolStripMenuItem,
+            this.cIAToolStripMenuItem,
+            this.dSToolStripMenuItem1});
+            this.dSToolStripMenuItem.Name = "dSToolStripMenuItem";
+            this.dSToolStripMenuItem.Size = new System.Drawing.Size(186, 38);
+            this.dSToolStripMenuItem.Text = "LPP R5";
+            // 
+            // dSXToolStripMenuItem
+            // 
+            this.dSXToolStripMenuItem.Name = "dSXToolStripMenuItem";
+            this.dSXToolStripMenuItem.Size = new System.Drawing.Size(172, 38);
+            this.dSXToolStripMenuItem.Text = "3DSX";
+            // 
+            // cIAToolStripMenuItem
+            // 
+            this.cIAToolStripMenuItem.Name = "cIAToolStripMenuItem";
+            this.cIAToolStripMenuItem.Size = new System.Drawing.Size(172, 38);
+            this.cIAToolStripMenuItem.Text = "CIA";
+            // 
+            // dSToolStripMenuItem1
+            // 
+            this.dSToolStripMenuItem1.Name = "dSToolStripMenuItem1";
+            this.dSToolStripMenuItem1.Size = new System.Drawing.Size(172, 38);
+            this.dSToolStripMenuItem1.Text = "3DS";
             // 
             // label2
             // 
@@ -85,7 +173,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(162, 76);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Build VPK";
+            this.button1.Text = "Build";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -93,7 +181,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(735, 167);
+            this.ClientSize = new System.Drawing.Size(761, 174);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
@@ -105,8 +193,10 @@
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Lua Player Plus Builder GUI v1.0 - gnmmarechal";
+            this.Text = "Lua Player Plus Builder GUI v1.1 - gnmmarechal";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,6 +210,16 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem targetPlaftormToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playStationVitaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hENkakuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lPPR4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nintendo3DSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dSXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cIAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dSToolStripMenuItem1;
     }
 }
 
